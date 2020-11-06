fd3aff5b5f4f77d51ecab6370b47e3c1398ca6c2
Kristyna Kupkova <kristynakupkova@gmail.com>
2020-11-06 11:32:42 -0500

    get unmapped sequences from BAM file

---
+++ getUnmappedSequences.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Goes through fastq files in a folder
+# 1) map to human 
+# 2) select the unmapped sequences - non-human stay 
+# 3) convert to fastq (the final fastq will have suffix corresponding to variable $nonHuman)
+
+nonHuman='_nonHuman'
+for i in *.fastq;
+ do 
+ 	# ID= everything before "."
+ 	id="$( cut -d '.' -f 1 <<< "$i" )"
+ 	echo $id.bam
+ 	
+ 	#map the FASTQ file to the human genome
+ 	bowtie2 -x /Users/lilcrusher/bowtie2-2.2.6/indexes/hg19/hg19  -p 6 --time -U $i | samtools view -S -b - > $id.bam
+ 	
+ 	# filter out the reads mapped to the human = only unmapped stay
+ 	samtools view -b -f 4 $id.bam > $id$nonHuman.bam
+ 	
+ 	#transform the noYeast BAM file to FASTQ file using bedtools
+ 	bedtools bamtofastq -i $id$nonHuman.bam -fq $id$nonHuman.fastq
+ 		
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
