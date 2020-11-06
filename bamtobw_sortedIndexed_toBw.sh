f11816c1e48954c7f3543c4c78b2a4fdad859fec
Kristyna Kupkova <kristynakupkova@gmail.com>
2020-11-06 11:25:21 -0500

    convert sorted BAM file to bigWig - hg19

---
+++ bamtobw_sortedIndexed_toBw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script will convert  sorted and indexed BAM files in the working directory to BIGWIG.
+#Note it specifies conversion of human genome data by directing to hg19chrom.sizes on local computer.
+
+#generate bed files:
+for i in *.sorted.bam; do bedtools bamtobed -i $i > $i.bed; done
+#generate bigwig files (for human):
+for i in *.bed; do echo $i; bedtools genomecov -i $i -g ~/gentools/chromsize/hg19chrom.sizes -bg > $i.cov; bedGraphToBigWig $i.cov ~/gentools/chromsize/hg19chrom.sizes $i.bw; done
