52e2b413c4e6140d626980fe06ca7fff282c5f97
Kristyna Kupkova <kristynakupkova@gmail.com>
2020-11-06 11:41:09 -0500

    find number of called peaks

---
+++ noOfPeaksMACS2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script goes through all bed files in a given folder and returns the number of 
+# lines in the BED file
+
+
+for i in *.broadPeak
+do 
+	id="$( cut -d '.' -f 1 <<< "$i" )"
+	echo $i
+
+	wc -l < $i
+done
+
+
+
+
+
+
---
+++ noOfSuperEnhancers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script goes through all bed files in a given folder and returns the number of 
+# lines in the BED file
+
+echo "Superenhancers \n"
+for i in *SuperEnhancers.bed
+do 
+	id="$( cut -d '.' -f 1 <<< "$i" )"
+	echo $i
+
+	wc -l < $i
+done
+
+
+echo "Enhancers \n"
+for i in *_Enhancers.bed
+do 
+	id="$( cut -d '.' -f 1 <<< "$i" )"
+	echo $i
+
+	wc -l < $i
+done
+
+
+
+
