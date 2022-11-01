3ac66de60c2ece695d30591e795cd081b1a3dea6
Kristyna Kupkova <kristynakupkova@gmail.com>
2022-11-01 18:37:45 -0400

    remove old unused

--- ybowtie2tobam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#This script will run Bowtie2 and then convert the resulting SAM file to BAM using YEAST (S. cerevisiae sacCer3) and FASTQ input files.
-echo -e What is the name of the input FASTQ file?
-read name1
-echo -e What prefix would you like added to the output BAM file?
-read name2
-bowtie2 -x /Users/beast/bowtie2-2.2.6/indexes/sacCer3.bt2/sacCer3 -p 3 --time -U $name1 | samtools view -S -b - > $name2.bam
-
--- ybowtietobam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#This script will run Bowtie and then convert the resulting SAM file to BAM using YEAST (S. cerevisiae sacCer3) and FASTQ input files.
-echo -e What is the name of the input FASTQ file?
-read name1
-echo -e What prefix would you like added to the output BAM file?
-read name2
-bowtie --sam  -q --nomaqround -m 3 --best --strata --chunkmbs 4000 --offrate 7 --seedmms 2 --threads 3 /Users/dauble/bowtie-1.0.0/indexes/sacCer3.ebwt/genome $name1 | samtools view -S -b - > $name2.bam
